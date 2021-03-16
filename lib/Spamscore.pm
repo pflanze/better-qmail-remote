92ec7ebdaf6f547918b7286b41ab9a11f111ec31
Christian Jaeger <ch@christianjaeger.ch>
2021-03-16 03:46:35 +0000

    qmail-remote,Spamscore: re-check emails if score is older than 30 seconds
    This is so that after passing mails that were delivered to spam
    folders through `HAM`, they can be `fwd`ed (without landing in the
    local mail dir).
    
    Also, this will spamcheck locally generated emails, just in case a
    forum is tied to mail, a local user is spamming, or similar.

--- lib/Spamscore.pm
+++ lib/Spamscore.pm
@@ -20,6 +20,7 @@ spamscore value.
 package Spamscore;
 @ISA="Exporter"; require Exporter;
 @EXPORT=qw(perhaps_wholemail_spamscore
+	   wholemail_spamscore
 	   perhaps_wholemail_doublebounce
 	   perhaps_wholemail_bounce
 	   perhaps_wholemail_spamscore_bounce
@@ -30,6 +31,10 @@ package Spamscore;
            mailheadstring_perhaps_headerfirstline
            );
 %EXPORT_TAGS=(all=>[@EXPORT,@EXPORT_OK]);
+
+use Date::Parse;  # always load, to avoid surprises, ok?
+use Chj::IO::Command;  # is always loaded by HashCash.pm anyway
+
 
 sub xcontentref {
     my ($path)=@_;
@@ -55,12 +60,81 @@ sub mailheadstring_perhaps_headerfirstline ($$) {
 }
 
 
-sub perhaps_wholemail_spamscore ($) {
-    #my ($str)=@_;
-    my $head= mailstring_xhead $_[0];
+sub mailheadstring_perhaps_received_unixtime {
+    my ($head)= @_;
+    if (my ($rcvd)= mailheadstring_perhaps_headerfirstline($head, "Received")) {
+	if (my ($timestr)= $rcvd=~ /^\s*\(qmail.*?\);\s*(.*)/) {
+	    my $t= Date::Parse::str2time $timestr;
+	    if (defined $t) {
+		$t
+	    } else {
+		warn "got invalid date string from qmail??: '$timestr'";
+		()
+	    }
+	} else {
+	    warn "expected Received value from qmail, got: '$rcvd'";
+	    ()
+	}
+    } else {
+	# warn "missing 'Received' header"--no, newly generated
+	# messages don't have one.
+	()
+    }
+}
+
+sub perhaps_mailheadstring_spamscore ($) {
+    my ($head)= @_;
     if (my ($score)= $head=~ /${NL}X-Spam-Status:.*score=(\S+)/) {
 	$score
     } else {
+	()
+    }
+}
+
+sub perhaps_wholemail_spamscore ($) {
+    my $head= mailstring_xhead $_[0];
+    perhaps_mailheadstring_spamscore $head
+}
+
+sub wholemail_spamscore {
+    # "Take spam score from header if present and recently delivered;
+    #  from `spamcheck` script instead otherwise. If spamcheck fails,
+    #  return nothing."
+    my $head= mailstring_xhead $_[0];
+
+    if (my ($received_t)= mailheadstring_perhaps_received_unixtime $head) {
+	if (abs(time - $received_t) < 30) {
+	    # There probably was no time to manually tag mail
+	    # (probably this is an auto-forwarding), thus assume that
+	    # the score didn't change.
+	    if (my ($spamscore)= perhaps_mailheadstring_spamscore $head) {
+		return $spamscore;
+	    }
+	}
+    }
+    # Fall back to running spamcheck and parsing *its* output.
+    my $res;
+    eval {
+	$res= do {
+	    require Chj::xtmpfile;
+	    my $t= Chj::xtmpfile::xtmpfile ("/tmp/better-qmail-remote_");
+	    $t->xprint($_[0]);
+	    $t->xclose;
+	    my $path= $t->path;
+	    my $in= Chj::IO::Command->new_sender("spamcheck", $path);
+	    my $str= $in->xcontent;
+	    $in->xxfinish; # must exit 0 in either case
+	    if (my ($newscore)= perhaps_wholemail_spamscore $str) {
+		$newscore
+	    } else {
+		warn "'spamcheck' script did not print an X-Spam-Status header";
+		# Visibly but safely break: assume it's spam (thus diverted to local).
+		10
+	    }
+	};
+	1
+    } || do {
+	warn "$0: wholemail_spamscore: exception: $@";
 	()
     }
 }
